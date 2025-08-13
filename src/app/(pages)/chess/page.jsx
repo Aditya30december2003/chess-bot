e4baeb9a3aa34bb50b9b2da64bfdf2062e4fefcd
Aditya30december2003 <adityasmjain@gmail.com>
2025-08-13 18:42:47 +0530

    made changes

--- src/app/(pages)/chess/page.jsx
+++ src/app/(pages)/chess/page.jsx
@@ -1,22 +1,12 @@
+// src/app/(pages)/chess/page.jsx
 'use client';
-import { useState } from 'react';
-import { Chess } from 'chess.js';
-// import ChessBoard from '../../components/chessBoard';
-// import BotSidebar from '../../components/BotSidebar';
-// import ChessMoveHistory from '../../components/ChessMoveHistory';
-import { useSearchParams } from 'next/navigation';
+
 import ChessGame from '../../components/chessBoard';
 
 export default function PlayPage() {
-  const searchParams = useSearchParams();
-  const playerName = searchParams.get('name') || 'Guest';
-  
-  const [game, setGame] = useState(new Chess());
-
   return (
     <div className="min-h-screen bg-[#d40202] relative">
-      <ChessGame/>
+      <ChessGame />
     </div>
   );
 }
-
